ae51c5c8f634ceef0a3599bf754ce4dfefa7a399
MiguelRegato <m.regato.2019@alumnos.urjc.es>
2024-10-15 10:44:07 +0200

    Delete unused files

--- TFGWordle/src/main/java/app/TFGWordle/controller/IndexController.java
+++ TFGWordle/src/main/java/app/TFGWordle/controller/IndexController.java
@@ -1,22 +1,8 @@
 package app.TFGWordle.controller;
 
-import app.TFGWordle.service.WordleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @Autowired
-    private WordleService wordleService;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-
-    @GetMapping("/wordle")
-    public String index() {
-        return "index";
-    }
 }
