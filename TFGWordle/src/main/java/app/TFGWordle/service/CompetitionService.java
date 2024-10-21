25d33c6614b6f5cd4551d4652ae8478b42f683e0
MiguelRegato <m.regato.2019@alumnos.urjc.es>
2024-10-21 14:58:22 +0200

    CompetitionServiceTest created

--- TFGWordle/src/main/java/app/TFGWordle/service/CompetitionService.java
+++ TFGWordle/src/main/java/app/TFGWordle/service/CompetitionService.java
@@ -7,10 +7,8 @@ import app.TFGWordle.security.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 public class CompetitionService {
