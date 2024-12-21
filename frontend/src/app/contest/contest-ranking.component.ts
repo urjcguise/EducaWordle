a0417f3ecb0209fb35b0926040749d144aa8512d
MiguelRegato <m.regato.2019@alumnos.urjc.es>
2024-12-21 11:05:47 +0100

    Update ranking to show more information

--- frontend/src/app/contest/contest-ranking.component.ts
+++ frontend/src/app/contest/contest-ranking.component.ts
@@ -14,9 +14,12 @@ export class ContestRankingComponent implements OnInit {
   contestName: string = '';
   contest!: Contest;
 
-  wordlesWithRankings: {
-    wordle: string;
-    rankings: { userName: string; score: number }[];
+  studentsRanking: {
+    name: string;
+    rightGuess: number;
+    actualTries: number[];
+    totalTries: number;
+    totalTime: number;
   }[] = [];
 
   constructor(private contestService: ContestService, private route: ActivatedRoute) { }
@@ -30,7 +33,6 @@ export class ContestRankingComponent implements OnInit {
           next: (userState) => {
             console.log(userState);
             this.obtainRanking(userState);
-            console.log(this.wordlesWithRankings);
           },
           error: (error) => {
             console.error('Error obteniendo los estados', error);
@@ -44,29 +46,37 @@ export class ContestRankingComponent implements OnInit {
   }
 
   obtainRanking(userState: UserState[]) {
-    const wordleRankingMap = new Map();
+    this.studentsRanking = [];
 
     userState.forEach((user) => {
       const userName = user.userName;
-
+      var numRightGuess = 0;
+      var totalRightGuess = 0;
+      var time = 0;
+      var tries: number[] = [];
       user.state.games.forEach((game) => {
         if (game.finished) {
-          const { wordle, tryCount, timeNeeded } = game;
-          const score = Number(timeNeeded) + tryCount;
-
-          if (!wordleRankingMap.has(wordle)) {
-            wordleRankingMap.set(wordle, []);
-          }
-
-          wordleRankingMap.get(wordle).push({ userName, score });
+          numRightGuess++;
+          time += Number(game.timeNeeded);
         }
+        tries = [game.tryCount, 6];
+        totalRightGuess += game.tryCount;
+      });
+      this.studentsRanking.push({
+        name: userName,
+        rightGuess: numRightGuess,
+        actualTries: tries,
+        totalTries: totalRightGuess,
+        totalTime: time,
       });
     });
+    this.studentsRanking.sort((a, b) => b.rightGuess - a.rightGuess);
+  }
 
-    this.wordlesWithRankings = Array.from(wordleRankingMap.entries()).map(([wordle, rankings]) => {
-      rankings.sort((a: { score: number; }, b: { score: number; }) => a.score - b.score);
-      return { wordle, rankings };
-    });
+  formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
   }
 }
 
